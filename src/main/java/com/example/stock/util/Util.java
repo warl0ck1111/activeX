4d34e4d65f25ea45e5d7f32da9a5e84abeb63a2d
Okala III <bashirokala@gmail.com>
2021-11-18 00:54:46 +0100

    added utility class for common functions

---
+++ src/main/java/com/example/stock/util/Util.java
@@ -0,0 +1,13 @@
+package com.example.stock.util;
+
+/**
+ * @author Okala III
+ */
+public class Util {
+    public static boolean hasValue(String s){
+        return (s != null) && !s.isEmpty() ;
+
+    }
+
+
+}
